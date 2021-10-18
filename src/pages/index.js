a827663b068e12355d23ee85f065b46aa6292593
tokenup077 <kentarokimura@m365ppdev.onmicrosoft.com>
2021-10-18 20:45:38 +0900

    part2 first

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home Page</title>
+      <h1>welcam to my gatsby site!</h1>
+      <p>I'm making this by following the gatsby tutorial.</p>
+      </main>      
+  )
+}
+
+export default IndexPage
