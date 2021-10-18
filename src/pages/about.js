3aca52796b26c3acb68afc36056da090bc999dd1
tokenup077 <kentarokimura@m365ppdev.onmicrosoft.com>
2021-10-18 21:44:56 +0900

    part2 add Link css

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,11 @@
 import * as React from 'react'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
     return(
-        <main>
-            <title>About Me</title>
-            <h1>About Me</h1>
+        <Layout pageTitle='About Me'>
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-        </main>
+        </Layout>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import * as React from "react"
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Home Page</title>
-      <h1>welcam to my gatsby site!</h1>
-      <p>I'm making this by following the gatsby tutorial.</p>
-      </main>      
+    <Layout pageTitle='Home Page'>
+          <p>I'm making this by following the gatsby tutorial.</p>
+    </Layout>     
   )
 }
 
